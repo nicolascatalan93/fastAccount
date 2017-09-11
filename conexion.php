bc7138eeb1944e75bbab3e2e099f64bef686826f
lockillohugo <31834090+lockillohugo@users.noreply.github.com>
2017-09-10 23:07:03 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,25 +1,4 @@
-<?php
-
-
-	class Conectar{
-
-
-		public function conexion(){
-			$user="root";
-			$pass="admin123";
-			$host="localhost";
-			$db="contalinedb";
-			try {
-				$conexion = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-				$conexion->serAttribute(PDO::ATTR_ERROMODE, PDO::ERROMODE_EXCEPTION);
-				$conexion->exec("SET CHARACTER SET UTF8");
-			} catch (Exception $e) {
-
-				die("Error". $e->getMessage());
-				echo "Linea del error" . $e->getLine();
-			}
-			return $conexion;
-		}
-	}
-
-?>
+public class Cuentas {
+   private Integer id;
+   private String 
+}
